d7420188d4adf1f1117e96b29d5eb0edb3951ebf
Youssef Elmarakshy <marakshy@Youssefs-MacBook-Pro.local>
2017-11-18 13:01:49 -0600

    commit

---
+++ ToDo/server/config/config.js
@@ -0,0 +1,20 @@
+var path = require('path'),    
+rootPath = path.normalize(__dirname + '/..'),    
+env = process.env.NODE_ENV || 'development';
+
+var config = {  
+development: {    
+            root: rootPath,    
+            app: {      name: 'ToDo'    },    
+            port: 5000,
+            db:'mongodb://127.0.0.1/todo-dev'
+             
+ },  
+ production: {    
+              root: rootPath,    
+              app: {      name: 'ToDo'    },    
+               port: 80,  }
+  };
+
+module.exports = config[env];
+//we use module.exports to import
