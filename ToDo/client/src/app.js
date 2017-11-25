c1ea10cd5a4b9506f954bf52ca75d16370566c9c
Youssef Elmarakshy <marakshy@youssefs-mbp.attlocal.net>
2017-11-25 13:41:44 -0600

    commit

--- ToDo/client/src/app.js
+++ ToDo/client/src/app.js
@@ -1,18 +1,5 @@
 export class App {
-  configureRouter(config, router) {
-    this.router = router;
-    config.map([
-      { 
-	route: ['', 'home'],
-	 moduleId: './modules/home',
-	 name: 'Home' 
-      },
-      
-      {
-	 route: 'list',
-	 moduleId: './modules/list',
-	 name: 'List' 
-     }
-    ]);
+  constructor() {
+    this.message = 'Hello World!';
   }
 }
